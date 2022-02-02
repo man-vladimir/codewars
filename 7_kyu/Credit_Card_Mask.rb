3c9f708be7485cd43195babe645959163ec707ce
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-02 17:33:54 +0200

    Refactor task

--- 7_kyu/Credit_Card_Mask.rb
+++ 7_kyu/Credit_Card_Mask.rb
@@ -1,6 +1,5 @@
 # https://www.codewars.com/kata/5412509bd436bd33920011bc
 
 def maskify(cc)
-  cc.length > 4 ? cc[0..-5] = "#" * (cc.length - 4) : cc
-  return cc
+  cc.size > 4 ? cc.last(4).unshift("#" * cc.size - 4) : cc
 end
