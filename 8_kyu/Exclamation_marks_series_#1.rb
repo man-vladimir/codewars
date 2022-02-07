86948a77ae12555aab1a831459163e2f0220793f
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-07 22:23:38 +0200

    Refactor task

--- 8_kyu/Exclamation_marks_series_#1.rb
+++ 8_kyu/Exclamation_marks_series_#1.rb
@@ -1,9 +1,5 @@
 # https://www.codewars.com/kata/57fae964d80daa229d000126/
 
-# def remove(s)
-#   s.gsub(/!\z/, "")
-# end
-
-def remove2(s)
-  s[-1] == "!" ? s.chop : s
+def remove(s)
+  s.gsub(/!\z/, "")
 end
