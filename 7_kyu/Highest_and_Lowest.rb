563ea90cd05b57559fe109f4860c5804b582dc14
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-12 11:21:26 +0200

    quote replacement mark

--- 7_kyu/Highest_and_Lowest.rb
+++ 7_kyu/Highest_and_Lowest.rb
@@ -1,11 +1,11 @@
 # https://www.codewars.com/kata/554b4ac871d6813a03000035
 
 def high_and_low_one(numbers) # version solution methods
-  numbers.split.map(&:to_i).minmax.reverse.join(" ")
+  numbers.split.map(&:to_i).minmax.reverse.join(' ')
 end
 
 def high_and_low_two(numbers) # version solution iterator
-  n = numbers.split(" ")
+  n = numbers.split(' ')
   min = n[0].to_i
   max = n[0].to_i
   n.each do |x|
