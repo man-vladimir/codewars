6fd292c965384efb1feaaad8b1677d3e47e581d3
Vladimir Manaenko <manaenko1@gmail.com>
2022-02-20 19:59:40 +0200

    Added second variant solution

--- 8_kyu/Sum_of_Multiples.rb
+++ 8_kyu/Sum_of_Multiples.rb
@@ -1,7 +1,12 @@
 # https://www.codewars.com/kata/57241e0f440cd279b5000829/
 
-def sum_mul(n, m)
+def sum_mul(n, m) # first variant
   return "INVALID" if n <= 0 || m <= 0 || m < n
   (n...m).to_a.select {|i| i if  i % n == 0 }.inject(:+)      
+end
+
+def sum_mul(n, m) # second variant
+  return "INVALID" if n <= 0 || m <= 0 || m < n
+  (n...m).step(n).inject(:+)   
 end
 
