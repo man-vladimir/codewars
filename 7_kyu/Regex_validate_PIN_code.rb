6e74e7cfe1c48a51b32bba5b533330d180586d18
Vladimir <manaenko1@gmail.com>
2022-11-15 20:13:15 +0200

    Added second variant solution

--- 7_kyu/Regex_validate_PIN_code.rb
+++ 7_kyu/Regex_validate_PIN_code.rb
@@ -1,8 +1,16 @@
 # https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
 
+# variant one
 def validate_pin(pin)
   
   /\A\d{4}\z|\A\d{6}\z/.match?(pin.to_s)  
   
+end
+
+# variant two
+def validate_pin(pin)
+
+  !!pin[/\A\d{4}\d{2}?\z/]
+ 
 end
 
