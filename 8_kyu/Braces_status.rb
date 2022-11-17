7b5e2fd24439d63d034a7bce08aae26feeca17a6
man-vladimir <97439971+man-vladimir@users.noreply.github.com>
2022-11-17 11:51:39 +0200

    Braces status (#27)
    * Braces status
    
    * Refactor solution

---
+++ 8_kyu/Braces_status.rb
@@ -0,0 +1,27 @@
+# https://www.codewars.com/kata/58983deb128a54b530000be6
+
+def braces_status(string)
+  stack = []
+  string.each_char do |char|
+    case char
+      when '(', '[', '{' then stack << char
+      when ')' then return false if stack.pop != '('
+      when ']' then return false if stack.pop != '['
+      when '}' then return false if stack.pop != '{'
+    end
+  end
+  stack.empty?
+end
+
+# refactor variant
+
+def braces_status(string)
+  braces, stack = {'('=>')', '['=>']', '{'=>'}'}, []
+  string.each_char do |char|
+    case char
+      when '(', '[', '{' then stack << char
+      when ')', ']', '}' then return false if stack.pop != braces.key(char)
+    end
+  end
+  stack.empty?
+end
