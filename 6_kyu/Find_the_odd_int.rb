22bdc8edae1b8124c536397438eb7227c56e4a98
Vladimir <manaenko1@gmail.com>
2022-11-25 11:52:25 +0200

    Added second variant solution

--- 6_kyu/Find_the_odd_int.rb
+++ 6_kyu/Find_the_odd_int.rb
@@ -1,7 +1,13 @@
 # https://www.codewars.com/kata/54da5a58ea159efa38000836
 
+# variant one
 def find_it(seq)
   resAll = seq.find_all { |el| seq.count(el) % 2 != 0 }   
   resAll[0] 
+end
+
+# variant two
+def find_it(seq)
+  seq. find { |el| seq.count(el).odd? }
 end
 
