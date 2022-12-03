130b033ec9e8c0e31c42677d2d3dbd9f46f6a12a
Vladimir <manaenko1@gmail.com>
2022-12-03 09:12:46 +0200

    Convert a string to an array

---
+++ 8_kyu/Convert_a_string_to_an_array.rb
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+# variant one
+def string_to_array(string)
+  string.split(" ")
+end
+
+# variant two
+def string_to_array(string)
+  string.split
+end
+
