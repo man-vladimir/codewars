26fbfbadae5607126b69c47325fbbe207e3638d1
Vladimir <manaenko1@gmail.com>
2023-02-17 09:14:38 +0200

    Filter out the geese

---
+++ 8_kyu/Filter_out_the_geese.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+
+def goose_filter (birds)
+  geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+  birds - geese
+end
+
